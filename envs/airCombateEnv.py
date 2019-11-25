05913f46f45d8ab32c9955aa32f49edeb670c7d7
wuzhaoxin <597779499@qq.com>
2019-11-25 19:54:05 +0800

    update envs

--- envs/airCombateEnv.py
+++ envs/airCombateEnv.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 import time
 import sys
+
 sys.path.append('..')
 from argument.dqnArgs import args
 from envs.units import REGISTRY as registry_unints
@@ -15,6 +16,70 @@ else:
     import tkinter as tk
 
 np.set_printoptions(suppress=True)
+np.random.seed(2)
+
+
+def net_frame_scen(init_scen , aircraftA, aircraftB):
+    """
+    根据初始想定模式为红蓝双方飞机初始化
+    :param init_scen:初始想定模式：0随机，1进攻，2防御，3同向，4中立
+    :param aircraftA:飞机A：被固定的一方
+    :param aircraftB:飞机B：随机生成的一方
+    """
+    if init_scen == 0:  # 随机
+        # 初始化A飞机
+        aircraftA.ac_pos = np.append(np.random.uniform(-500, 500),
+                                     np.random.uniform(-250, 250))
+        aircraftA.ac_heading = np.random.uniform(0, 360)
+        # 初始化B飞机
+        aircraftB.ac_pos = np.append(np.random.uniform(-500, 500),
+                                     np.random.uniform(-250, 250))
+        aircraftB.ac_heading = np.random.uniform(0, 360)
+    elif init_scen is 1:
+        # 初始化A飞机
+        aircraftA.ac_pos = np.array([0.0, 0.0])  # 二维坐标
+        aircraftA.ac_heading = 0  # 朝向角,向东
+        # 初始化B飞机
+        aircraftB.ac_pos = np.append(np.random.uniform(-100, -500),
+                                     np.random.uniform(-50, 50))
+        aircraftB.ac_heading = np.random.uniform(0, 60)
+    elif init_scen == 2:
+        # 初始化A飞机
+        aircraftA.ac_pos = np.array([0.0, 0.0])  # 二维坐标
+        aircraftA.ac_heading = 0  # 朝向角,向东
+        # 初始化B飞机
+        aircraftB.ac_pos = np.append(np.random.uniform(100, 500),
+                                     np.random.uniform(-50, 50))
+        aircraftB.ac_heading = np.random.uniform(0, 30)
+    elif init_scen == 3:
+        # 初始化A飞机
+        aircraftA.ac_pos = np.array([0.0, 0.0])  # 二维坐标
+        aircraftA.ac_heading = 0  # 朝向角,向东
+        # 初始化B飞机
+        aircraftB.ac_pos = np.append(np.random.uniform(100, 500),
+                                     np.random.uniform(-50, 50))
+        aircraftB.ac_heading = np.random.uniform(150, 180)
+    elif init_scen == 4:
+        # 初始化A飞机
+        aircraftA.ac_pos = np.array([0.0, 0.0])  # 二维坐标
+        aircraftA.ac_heading = 0  # 朝向角,向东
+        # 初始化B飞机
+        aircraftB.ac_pos = np.append(np.random.uniform(-50, 50),
+                                     np.random.uniform(-250, 250))
+        aircraftB.ac_heading = 0
+    else:
+        raise Exception("init_scen error")
+    aircraftA.ac_bank_angle = 0     # 滚转角
+    aircraftB.ac_bank_angle = 0
+    aircraftA.oil = args.Sum_Oil     # 油量
+    aircraftB.oil = args.Sum_Oil
+    return aircraftA, aircraftB
+
+# 构建状态空间的函数
+
+REGISTRY = {}
+REGISTRY["scen"] = net_frame_scen
+
 
 class Env(object):
     def __init__(self):
@@ -38,7 +103,7 @@ class Env(object):
         self.red_unit_list = []
         self.blue_unit_list = []
 
-    def _seed(self,seed):
+    def _seed(self, seed):
         '''
         设置随机种子
         '''
@@ -64,7 +129,7 @@ class Env(object):
         '''
         raise NotImplementedError
 
-    def _get_reward(self,*args):
+    def _get_reward(self, *args):
         '''
         根据当前飞机信息，给出奖励函数。
         最好是输入设置为当前环境能够给出的所有信息，返回的是奖励值。
@@ -102,8 +167,249 @@ class Env(object):
     def _get_avail_actions(self):
         raise NotImplementedError
 
-    def _get_agent_avail_actions(self, agent_id):        
-        raise NotImplementedError   
+    def _get_agent_avail_actions(self, agent_id):
+        raise NotImplementedError
+
+
+class AirCombatEnv(Env):
+    def __init__(self):
+        super(AirCombatEnv, self).__init__()
+        # 飞机参数
+        self.red = registry_unints["default"](None, 200, 80)  # 红方飞机
+        self.blue = registry_unints["default"](None, 200, 80)  # 蓝方飞机
+        # reward判断指标
+        self.AA_range = 60  # 视界角范围
+        self.ATA_range = 30  # 天线拂擦角范围
+        self.Dis_max = 500  # 距离最大值
+        self.Dis_min = 100  # 距离最小值
+        self.adv_count = 0  # 持续建立优势的次数
+        # 初始想定模式：0随机，1进攻，2防御，3同向，4中立
+        self.init_scen = 0
+        # 强化学习动作接口
+        self.action_space = ['l', 's', 'r']  # 向左滚转、维持滚转、向右滚转
+        self.n_actions = len(self.action_space)
+        self.action_dim = self.n_actions
+        self.state_dim = len(self._get_state(self.red,self.blue,self.adv_count))
+        # reward条件
+        self.success = 0
+
+    def reset_selfPlay(self):
+        # 初始化参数
+        self.reward_b = 0
+        self.reward_r = 0
+        self.done = False
+        self.success = 0
+        self.acts = [[], []]
+        self.advs = []
+        self.ATA_b = self.AA_b = 100
+        self.ATA_r = self.AA_r = 100
+        self.adv_count = 0
+        # 初始化红蓝方飞机
+        REGISTRY["scen"](self.init_scen, self.red, self.blue)
+        #计算ATA，AA
+        self.ATA_b, self.AA_b = self._getAngle(self.red.ac_pos, self.blue.ac_pos, self.red.ac_heading,
+                                               self.blue.ac_heading)
+        self.ATA_r, self.AA_r = self._getAngle(self.blue.ac_pos, self.red.ac_pos, self.blue.ac_heading,
+                                               self.red.ac_heading)
+        # 计算距离
+        dis = self._get_dis(self.red.ac_pos,self.blue.ac_pos)
+        # 计算优势
+        self.adv_count = self._calculate_Advantages(self.adv_count, dis, self.AA_r,self.ATA_r,self.AA_b,self.ATA_b)
+        # reward shaping
+        RA_b = 1 - ((1 - math.fabs(self.ATA_b) / 180) + (1 - math.fabs(self.AA_b) / 180))
+        RA_r = 1 - ((1 - math.fabs(self.ATA_r) / 180) + (1 - math.fabs(self.AA_r) / 180))
+        RD = math.exp(-(math.fabs(dis - ((self.Dis_max + self.Dis_min) / 2)) / 180 * 0.1))
+        Rbl_b = RA_b * RD
+        Rbl_r = RA_r * RD
+        self.fai_b = -0.01 * Rbl_b
+        self.fai_r = -0.01 * Rbl_r
+        # 返回红蓝飞机状态
+        s_b = self._get_state(self.red,self.blue,self.adv_count)
+        s_r = self._get_state(self.blue,self.red,self.adv_count)
+        return s_b, s_r
+
+    # levin - [done]： add both actions
+    def step_selfPlay(self, action_b, action_r):
+        # 记录动作
+        self.acts[0].append(action_b)
+        self.acts[1].append(action_r)
+        # 执行动作
+        self.blue.move(action_b)
+        self.red.move(action_r)
+        # 返回红蓝飞机状态
+        s_b = self._get_state(self.red, self.blue, self.adv_count)
+        s_r = self._get_state(self.blue, self.red, self.adv_count)
+        # 计算reward
+        self.reward_b, self.reward_r, self.done = self._get_reward(self.red.ac_pos, self.red.ac_heading,
+                                                    self.blue.ac_pos, self.blue.ac_heading, self.adv_count)
+        return s_b, s_r, self.reward_b, self.reward_r, self.done
+
+    def _getAngle(self, pos_r, pos_b, heading_r, heading_b):
+        """
+        计算AA和ATA
+        :param pos_r: A方坐标
+        :param pos_b: B方坐标
+        :param heading_r: A方朝向
+        :param heading_b: B方朝向
+        :return:B的AA和ATA角
+        """
+        theta_br = 180 * math.atan2((pos_r[1] - pos_b[1]), (pos_r[0] - pos_b[0])) / math.pi
+        theta_rb = 180 * math.atan2((pos_b[1] - pos_r[1]), (pos_b[0] - pos_r[0])) / math.pi
+        if theta_br < 0:
+            theta_br = 360 + theta_br
+        if theta_rb < 0:
+            theta_rb = 360 + theta_rb
+        ATA = heading_b - theta_br
+        AA = 180 + heading_r - theta_rb
+        if ATA > 180:
+            ATA = 360 - ATA
+        elif ATA < -180:
+            ATA = 360 + ATA
+        if AA > 180:
+            AA = 360 - AA
+        elif AA < -180:
+            AA = 360 + AA
+        return ATA, AA
+
+    def _get_reward(self, ac_pos_r, ac_heading_r, ac_pos_b, ac_heading_b, adv_count):
+        dis = math.sqrt((ac_pos_r[0] - ac_pos_b[0]) * (ac_pos_r[0] - ac_pos_b[0])
+                        + (ac_pos_r[1] - ac_pos_b[1]) * (ac_pos_r[1] - ac_pos_b[1]))
+        # 计算ATA和AA
+        self.ATA_b, self.AA_b = self._getAngle(ac_pos_r, ac_pos_b, ac_heading_r, ac_heading_b)
+        self.ATA_r, self.AA_r = self._getAngle(ac_pos_b, ac_pos_r, ac_heading_b, ac_heading_r)
+        # 计算优势
+        adv_count = self._calculate_Advantages(adv_count, dis, self.AA_r,self.ATA_r,self.AA_b,self.ATA_b)
+        # reward shaping
+        RA_b = 1 - ((1 - math.fabs(self.ATA_b) / 180) + (1 - math.fabs(self.AA_b) / 180))
+        RA_r = 1 - ((1 - math.fabs(self.ATA_r) / 180) + (1 - math.fabs(self.AA_r) / 180))
+        RD = math.exp(-(math.fabs(dis - ((self.Dis_max + self.Dis_min) / 2)) / 180 * 0.1))
+        Rbl_b = RA_b * RD
+        Rbl_r = RA_r * RD
+        self.old_fai_b = self.fai_b
+        self.old_fai_r = self.fai_r
+        self.fai_b = -0.01 * Rbl_b
+        self.fai_r = -0.01 * Rbl_r
+        # 计算reward和终止条件
+        if adv_count >= 9:
+            done = True
+            self.success = 1
+            reward_b = 2.0
+            reward_r = -2.0
+        elif adv_count <= -9:
+            done = True
+            self.success = -1
+            reward_b = -2.0
+            reward_r = 2.0
+        elif self.red.oil <= 0 and self.blue.oil <= 0:
+            done = True
+            self.success = 0
+            reward_b = -1.0
+            reward_r = -1.0
+        elif (ac_pos_b[0] > args.map_area) or ((0 - ac_pos_b[0]) > args.map_area) or \
+                (ac_pos_b[1] > args.map_area) or ((0 - ac_pos_b[1]) > args.map_area):
+            done = True
+            self.success = 0
+            reward_b = -1.0
+            reward_r = (self.fai_r - self.old_fai_r) - 0.001
+        elif (ac_pos_r[0] > args.map_area) or ((0 - ac_pos_r[0]) > args.map_area) or \
+                (ac_pos_r[1] > args.map_area) or ((0 - ac_pos_r[1]) > args.map_area):
+            done = True
+            self.success = 0
+            reward_b = (self.fai_b - self.old_fai_b) - 0.001
+            reward_r = -1.0
+        else:
+            done = False
+            reward_b = (self.fai_b - self.old_fai_b) - 0.001
+            reward_r = (self.fai_r - self.old_fai_r) - 0.001
+        return reward_b, reward_r, done
+
+    def _calculate_Advantages(self, adv_count, dis, AA_r, ATA_r, AA_b, ATA_b):
+        """
+        计算红蓝方优势次数
+        :param adv_count:优势次数：-1为红方优势态势，1为蓝方优势态势
+        :param dis:距离
+        :param AA_r:红方AA角
+        :param ATA_r:红方ATA角
+        :param AA_b:蓝方AA角
+        :param ATA_b:蓝方ATA角
+        :return:优势次数
+        """
+        # 计算优势
+        if (dis < self.Dis_max) and (dis > self.Dis_min) \
+                and (abs(AA_b) < self.AA_range) and (abs(ATA_b) < self.ATA_range):
+            if adv_count >= 0:  # 如果之前蓝方已经是优势态势，优势累加
+                adv_count += 1
+            else:  # 如果之前是红方优势态势，优势交替
+                adv_count = 1
+        elif (dis < self.Dis_max) and (dis > self.Dis_min) \
+                and (abs(AA_r) < self.AA_range) and (abs(ATA_r) < self.ATA_range):
+            if adv_count <= 0:
+                adv_count -= 1
+            else:
+                adv_count = -1
+        else:
+            adv_count = 0
+        self.advs.append(adv_count)
+        return  adv_count
+
+    def _get_dis(self, pos_a, pos_b):
+        """
+        计算坐标A和坐标B的距离
+        :param pos_a: 坐标A
+        :param pos_b: 坐标B
+        :return: 坐标A和坐标B的距离
+        """
+        dis = math.sqrt((pos_a[0] - pos_b[0]) * (pos_a[0] - pos_b[0])
+                        + (pos_a[1] - pos_b[1]) * (pos_a[1] - pos_b[1]))
+        return  dis
+
+    def _get_state(self, aircraft_a, aircraft_b, adv_count):
+        """
+        计算aircraft_b的状态
+        :param aircraft_a:
+        :param aircraft_b:
+        :param adv_count:优势次数
+        :return:aircraft_b的状态
+        """
+        state = np.concatenate(((aircraft_b.ac_pos - aircraft_a.ac_pos) / args.map_area,
+                                [aircraft_b.ac_heading / 180, aircraft_a.ac_heading / 180,
+                                 aircraft_b.ac_bank_angle / 80, adv_count / 10]))
+        return  state
+
+    def creat_ALG(self):
+        self.Tk = tk.Tk()
+        self.Tk.title('1V1')
+        self.Tk.canvas = tk.Canvas(self.Tk, bg='white',
+                                   height=args.map_area * args.map_scale * 2,
+                                   width=args.map_area * args.map_scale * 2)
+        self.Tk.canvas.pack()
+
+    def render(self):
+        # 刷新红方飞机
+        self.r_show = self.xyz2abc(self.red.ac_pos)
+        self.r = self.Tk.canvas.create_oval(
+            self.r_show[0] - 1, self.r_show[1] - 1,
+            self.r_show[0] + 1, self.r_show[1] + 1,
+            fill='red')
+
+        # 刷新蓝方飞机
+        self.b_show = self.xyz2abc(self.blue.ac_pos)
+        self.b = self.Tk.canvas.create_oval(
+            self.b_show[0] - 1, self.b_show[1] - 1,
+            self.b_show[0] + 1, self.b_show[1] + 1,
+            fill='blue')
+
+        self.Tk.update()
+        time.sleep(0.05)
+        if self.done:
+            time.sleep(0.1)
+            self.Tk.destroy()
+
+    def xyz2abc(self, pos):
+        pos_show = np.array([0, 0])
+        pos_show[0] = pos[0] * args.map_scale + args.map_area * args.map_scale
+        pos_show[1] = args.map_area * args.map_scale - pos[1] * args.map_scale
+        return pos_show
 
 
 class AirCombatEnvMultiUnit(Env):
@@ -112,14 +418,14 @@ class AirCombatEnvMultiUnit(Env):
 
         # 初始化双方飞机
         id_number = 0
-        for name in args.red_unit_type_list:        # args.red_unit_type_list为飞机类型名字列表
-            red_unit = registry_unints[name](id_number)  
+        for name in args.red_unit_type_list:  # args.red_unit_type_list为飞机类型名字列表
+            red_unit = registry_unints[name](id_number)
             self.red_unit_list.append(red_unit)
             id_number = id_number + 1
 
         id_number = 0
-        for name in args.blue_unit_type_list:        
-            blue_unit = registry_unints[name](id_number)  
+        for name in args.blue_unit_type_list:
+            blue_unit = registry_unints[name](id_number)
             self.blue_unit_list.append(blue_unit)
             id_number = id_number + 1
 
@@ -135,33 +441,30 @@ class AirCombatEnvMultiUnit(Env):
         # self.action_space = ['l', 's', 'r']
 
         self.action_dim = self.n_actions = len(self.action_space)
-        self.state_dim = 5                   # 需要定义
+        self.state_dim = 5  # 需要定义
 
         # todo：reward判断指标
 
-        
     def reset_selfPlay(self):
         self.done = False
         self.success = 0
         self.acts = [[], []]  # todo-levin: 只保存一个agent的acts，还是两个acgent的acts？
 
         # 1°初始化红蓝红方飞机
-            # todo: 
-            # 【方案一】：
-            # 按照实际阵型构建初始化阵型函数，初始化中心飞机的位置，确定每个飞机的位置
-            # 【方案二】：
-            # 随机初始化一个区域，在区域内随机初始各飞机的位置
+        # todo:
+        # 【方案一】：
+        # 按照实际阵型构建初始化阵型函数，初始化中心飞机的位置，确定每个飞机的位置
+        # 【方案二】：
+        # 随机初始化一个区域，在区域内随机初始各飞机的位置
 
-            # 遍历列表初始化飞机的状态：滚转角、油量等
-            
+        # 遍历列表初始化飞机的状态：滚转角、油量等
+
         # 2°得到初始状态
         for unit in self.red_unit_list:
             # 分别得到红、蓝双方的联结状态空间
             pass
         for unit in self.blue_unit_list:
             pass
-
-        return s_b, s_r
 
     # levin - [done]： add both actions
     def step_selfPlay(self, action_blue_list, action_red_list):
@@ -174,12 +477,45 @@ class AirCombatEnvMultiUnit(Env):
         self._unit_move(self.blue_unit_list, action_blue_list)
         self._unit_move(self.red_unit_list, action_red_list)
 
-       # 2° todo：得到联结状态空间
-       # 3° todo：得到奖励值和done
+    # 2° todo：得到联结状态空间
+    # 3° todo：得到奖励值和done
 
     def _unit_move(self, unit_list, action_list):
         # todo：判断unit_list和action_list的维度匹配
         for unit, action in zip(unit_list, action_list):
             unit.move(action)
-            
-        
+
+
+class AirCombatEnvOverload(Env):
+    def __init__(self):
+        super(AirCombatEnv, self).__init__()
+        # 飞机参数
+        self.red = registry_unints["overload"]()  # 红方飞机
+        # reward判断指标
+        self.Dis_max = 500  # 距离最大值
+        self.Dis_min = 100  # 距离最小值
+        # 强化学习动作接口
+        self.action_space = ['0', '1', '2', '3', '4', '5', '6']  # 向左滚转、维持滚转、向右滚转
+        self.n_actions = len(self.action_space) 
+        self.action_dim = self.n_actions
+        self.state_dim = len(self._get_state(self.red, self.blue, self.adv_count))
+        # reward条件
+        self.success = 0
+
+    def reset_selfPlay(self):
+        # 初始化参数
+        self.reward = 0
+        self.done = False
+        self.success = 0
+        self.acts = []
+        # 初始化飞机位置和航母位置
+
+        # 计算距离
+        dis = self._get_dis(self.red.ac_pos,self.blue.ac_pos)
+
+        # 返回红蓝飞机状态
+        s_b = self._get_state(self.red,self.blue,self.adv_count)
+        s_r = self._get_state(self.blue,self.red,self.adv_count)
+        return s_b, s_r
+
+    def _init_pos(self):
---
+++ envs/units.py
@@ -0,0 +1,217 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+定义各种飞机及其他单位信息:
+定义每种类型飞机的 锁定区域半径、状态格式、（锁定时间）、（可视范围）、动作空间及运动模型等
+''' 
+import math
+import numpy as np
+import sys
+sys.path.append('..')
+from argument.dqnArgs import args
+
+class Aircraft(object):
+    def __init__(self, id_number=None):
+        '''
+        需要定义飞机的属性，飞机的编号
+        考虑：每个单位的动作空间，状态空间
+        '''
+        raise NotImplementedError 
+
+    def move(self, action):
+        '''
+        使用动力学模型：_aricraft_dynamic
+            输入：动作
+            输出：位置，朝向角等
+        '''
+        raise NotImplementedError
+
+    def get_oil(self):
+        '''
+        剩余油量
+        '''
+        raise NotImplementedError
+
+    def attack_range(self):
+        '''
+        攻击范围
+        '''
+        raise NotImplementedError
+
+    def locking_time(self):
+        '''
+        锁定时间
+        '''
+        raise NotImplementedError     
+
+
+# 子飞机类型设计
+class AircraftDefault(Aircraft):
+
+    def __init__(self, id=None, ac_speed=200, ac_bank_angle_max=80):
+        self.ac_pos = np.array([0.0, 0.0])                                  # 二维坐标
+        self.ac_heading = 0                                               # 朝向角,向南
+        self.ac_bank_angle = 0                                              # 滚转角一开始应该都为0
+        self.oil = args.Sum_Oil
+        self.ac_bank_angle_max = ac_bank_angle_max                          # φ_max
+        self.ac_speed = ac_speed                                            # 飞机速度，m/s
+        self.td = args.map_t / args.map_t_n
+
+    def move(self, action):
+        for i in range(args.map_t_n):
+            #飞机运动计算
+            self.ac_bank_angle = self.ac_bank_angle + (action - 1) * args.roll_rate * self.td
+            self.ac_bank_angle = max(self.ac_bank_angle,-self.ac_bank_angle_max)
+            self.ac_bank_angle = min(self.ac_bank_angle, self.ac_bank_angle_max)
+            self.turn_rate = (args.G / self.ac_speed) * math.tan(self.ac_bank_angle * math.pi / 180) * 180 / math.pi
+            self.ac_heading = self.ac_heading - self.turn_rate * self.td
+            if self.ac_heading > 360:
+                self.ac_heading -= 360
+            elif self.ac_heading < 0:
+                self.ac_heading += 360
+            self.ac_pos[0] = self.ac_pos[0] + self.ac_speed * self.td * math.cos(self.ac_heading * math.pi / 180)
+            self.ac_pos[1] = self.ac_pos[1] + self.ac_speed * self.td * math.sin(self.ac_heading * math.pi / 180)
+        self.oil -= 1
+
+    def get_oil(self):
+        return self.oil
+class AircraftOverload(Aircraft):
+    def __init__(self, ac_speed=150):
+        self.ac_pos = np.array([0.0, 0.0, 4000.0])  # 三维坐标
+        self.ac_speed = ac_speed  # 飞机速度，m/s
+        self.ac_speed_min = 100
+        self.ac_speed_max = 300
+        self.ac_heading = 0  # 朝向角/偏角
+        self.ac_pitch = 0  # 俯仰角/倾角
+        self.ac_pitch_max = 60
+        self.ac_roll = 0  # 滚转角
+        self.rate_roll = 40  # 滚转角变化率
+        self.roll_max = 80
+        self.t = 0.5
+        self.nx = 0  # 切向过载
+        self.ny = 1  # 过载
+        self.nz = 0  # 过载
+        self.nf = 5  # 法向过载
+        self.list = []
+        self.x = []
+        self.y = []
+        self.z = []
+
+
+    def move(self, action):
+        ##### 限定条件
+        if (self.ac_speed < 100 or self.ac_speed > 300):
+            return -1
+        elif (self.ac_pos[0] > 40000 or self.ac_pos[0] < -40000
+              or self.ac_pos[1] > 40000 or self.ac_pos[1] < -40000
+              or self.ac_pos[2] > 8000 or self.ac_pos[2] < 0):
+            return -1
+        #####
+
+        self.list.append(action)
+        for i in range(1, 9):
+            self._overload(action)
+            a, rate_pitch, rate_heading = self._get_rate(self.nx, self.ny, self.nz, self.ac_pitch, G, self.ac_speed)
+            self.ac_speed = self.ac_speed + a * self.t
+            ##### 检查速度
+            self.ac_speed = max(self.ac_speed,self.ac_speed_min)
+            self.ac_speed = min(self.ac_speed,self.ac_speed_max)
+            #####
+            last_pitch = self.ac_pitch
+            self.ac_pitch = self.ac_pitch + rate_pitch * 180 * self.t / math.pi
+            ##### 检查倾角 /////////记录变化之前和变化之后相乘为负即归零，判断是不是爬升俯冲
+            if last_pitch * self.ac_pitch < 0 and (action != 3 or action != 4):
+                self.ac_pitch = 0
+            if self.ac_pitch > 180:
+                self.ac_pitch = self.ac_pitch - 360
+            elif self.ac_pitch < -180:
+                self.ac_pitch = self.ac_pitch + 360
+            #####
+            self.ac_heading = self.ac_heading + rate_heading * 180 * self.t / math.pi
+            last_roll = self.ac_roll
+            self.ac_roll = self.ac_roll + self.rate_roll * self.t
+            ##### 检查滚转角
+            self.ac_roll = max(self.ac_roll, -self.roll_max)
+            self.ac_roll = min(self.ac_roll, self.roll_max)
+            if last_roll * self.ac_roll < 0 and (action != 1 or action != 2):
+                self.ac_pitch = 0
+            #####
+
+            self.ac_pos[0] = self.ac_pos[0] + self.ac_speed * math.cos(self.ac_pitch * math.pi / 180) * math.cos(
+                self.ac_heading * math.pi / 180)
+            self.ac_pos[1] = self.ac_pos[1] + -1 * self.ac_speed * math.cos(self.ac_pitch * math.pi / 180) * math.sin(
+                self.ac_heading * math.pi / 180)
+            self.ac_pos[2] = self.ac_pos[2] + self.ac_speed * math.sin(self.ac_pitch * math.pi / 180)
+            print(self.ac_pos[0], self.ac_pos[1], self.ac_pos[2], self.ac_heading, self.ac_pitch, self.ac_roll, self.ac_speed)
+            self.x.append(self.ac_pos[0])
+            self.y.append(self.ac_pos[1])
+            self.z.append(self.ac_pos[2])
+
+    def _get_rate(self, nx, ny, nz, pitch, g, v):
+        a = g * (nx - math.sin(pitch * math.pi / 180))                          # 飞机加速度
+        rate_pitch = (ny - math.cos(pitch * math.pi / 180)) * g / v             # 倾角的变化率
+        if pitch == 90 or pitch == -90:                                         # θ = 90 or -90
+            rate_heading = 0
+        else:
+            rate_heading = 1 * nz * g / (v * math.cos(pitch * math.pi / 180))  # 偏角的变化率
+        return a, rate_pitch, rate_heading
+
+    def _overload(self,action):
+        ##### 检查滚转角
+        if self.ac_roll < 0:                    # 刚执行完左转动作，需要将滚转角归零
+            self.rate_roll = 40
+        elif self.ac_roll > 0:
+            self.rate_roll = -40                # 刚执行完右转动作，需要将滚转角归零
+        else:
+            self.rate_roll = 0                  # 滚转角为0，无需改变
+        ##### 检查倾角
+        if self.ac_pitch < 0:                   # 刚执行完俯冲动作，需要将倾角归零
+            self.ny = self.nf * math.cos(self.ac_roll * math.pi / 180)
+        elif self.ac_pitch > 0:                 # 刚执行完爬升动作，需要将倾角归零
+            self.ny = - self.nf * math.cos(self.ac_roll * math.pi / 180)
+        else:
+            self.ny = math.cos(self.ac_pitch * math.pi / 180)     # 倾角为0，rate_θ = 0
+        #####
+        ##### 检查nz
+        if self.ac_pitch == 0:
+            self.nz = 0
+        else:
+            self.nz = self.nf * math.sin(self.ac_roll * math.pi / 180)
+        #####
+        if action == 0:             # 稳定飞行
+            self.nx = math.sin(self.ac_pitch * math.pi / 180)  # 切向过载
+        elif action == 1:           # 最大过载左转
+            self.nx = math.sin(self.ac_pitch * math.pi / 180)
+            if self.ac_pitch == 0:
+                self.nz = -math.sqrt(self.nf * self.nf - self.ny * self.ny)
+            self.rate_roll = -40
+        elif action == 2:           # 最大过载右转
+            self.nx = math.sin(self.ac_pitch * math.pi / 180)
+            if self.ac_pitch == 0:
+                self.nz = math.sqrt(self.nf * self.nf - self.ny * self.ny)
+            self.rate_roll = 40
+        elif action == 3:           # 最大过载爬升
+            self.nx = math.sin(self.ac_pitch * math.pi / 180)
+            self.ny = self.nf * math.cos(self.ac_roll * math.pi / 180)
+        elif (action == 4):         # 最大过载俯冲
+            self.nx = math.sin(self.ac_pitch * math.pi / 180)
+            self.ny = -self.nf * math.cos(self.ac_roll * math.pi / 180)
+        elif (action == 5):         # 最大加速
+            self.nx = 2
+        elif (action == 6):         # 最大减速
+            self.nx = -2
+
+    def show(self):
+        from matplotlib import pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = plt.figure()
+        ax1 = plt.axes(projection='3d')
+        ax1.plot3D(self.x, self.y, self.z, 'gray')  # 绘制空间曲线
+        plt.show()
+
+
+REGISTRY = {}
+REGISTRY["default"] = AircraftDefault
+REGISTRY["overload"] = AircraftOverload
+#REGISTRY["name_new"] = NewClass
