932c161b133e71be6d464fed4e4580873a0adbbb
MatyTanQ <1396568121@qq.com>
2019-12-07 15:40:56 +0800

    modified the config and add argManage

--- argument/argManage.py
+++ argument/argManage.py
@@ -1,33 +1,12 @@
 import sys
 sys.path.append("..")
-from common.config import merge
-from common.utlis import judge_type
+from common.config import merge,args_wrapper_parser,args_wrapper_path
+
 import os
 import argparse
 
 param = {'env': 'airCombateEnv', 'algs': 'dqn', 'memory': 'memory'}  # memory不用的时候value为None即可
 args_origin = merge(param)
-
-
-def args_wrapper_parser(args):
-    '''
-    将字典形式的参数解析到 argparse 中，方便命令行传参
-    '''
-    parser = argparse.ArgumentParser()
-    for key, value in args.items():
-        eval("parser.add_argument('--%s', type=%s, default='%s')"%(key, judge_type(value), value))
-    args = parser.parse_args()
-    return args
-
-def args_wrapper_path(args):
-    '''
-    主要是对重复训练的保存路径进行封装
-    '''
-    args.save_path = args.source_path + '/' + args.experiment_name + '/'
-    if not os.path.exists(args.save_path):
-        os.makedirs(args.save_path)
-    return args
-
 args_after_parse = args_wrapper_parser(args_origin)
 args = args_wrapper_path(args_after_parse)
 # print(args)
--- common/config.py
+++ common/config.py
@@ -2,6 +2,8 @@ import numpy as np
 import yaml
 from easydict import EasyDict as edict
 import os
+import argparse
+from common.utlis import judge_type
 base_dir = os.path.dirname(os.path.dirname(__file__))
 #print(base_dir)
 dict = edict()
@@ -24,6 +26,25 @@ def merge(param):
             dict.update(cfg_from_file(key,value))
    # print(dict)
     return dict
+
+def args_wrapper_parser(args):
+    '''
+    将字典形式的参数解析到 argparse 中，方便命令行传参
+    '''
+    parser = argparse.ArgumentParser()
+    for key, value in args.items():
+        eval("parser.add_argument('--%s', type=%s, default='%s')"%(key, judge_type(value), value))
+    args = parser.parse_args()
+    return args
+
+def args_wrapper_path(args):
+    '''
+    主要是对重复训练的保存路径进行封装
+    '''
+    args.save_path = args.source_path + '/' + args.experiment_name + '/'
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+    return args
 # def cfg_from_file(filename,subfolder):
 #     if subfolder==None:
 #         with open(os.path.join(base_dir, "config","{}.yaml".format(filename)), 'r', encoding='utf-8') as f:
