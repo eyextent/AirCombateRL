3a829914cb1ab1093a78742157715a86b042c4cd
wuzhaoxin <597779499@qq.com>
2019-12-09 10:59:15 +0800

    modified pos

--- envs/airCombateEnv/customization.py
+++ envs/airCombateEnv/customization.py
@@ -15,118 +15,112 @@ def random_pos(init_scen, red, blue, random_r, random_b):
     :return:red, blue
     """
     if init_scen == 0:  # 随机
-        if random_r == 0 and random_b == 1:  # red不随机,blue随机
-            red.ac_pos = np.array([0.0, 0.0])  # 二维坐标
+        if random_r == 1:
+            red.ac_pos = np.append(np.random.uniform(-500, 500),
+                                   np.random.uniform(-250, 250))
+            red.ac_heading = np.random.uniform(0, 360)
+        elif random_r == 0:
+            red.ac_pos = np.array([100.0, 0.0])  # 二维坐标
             red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.append(np.random.uniform(-500, 500),
-                                    np.random.uniform(-250, 250))
-            blue.ac_heading = np.random.uniform(0, 360)
-        elif random_r == 1 and random_b == 0:  # red随机,blue不随机
-            red.ac_pos = np.append(np.random.uniform(-500, 500),
-                                   np.random.uniform(-250, 250))
-            red.ac_heading = np.random.uniform(0, 360)
-            blue.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            blue.ac_heading = 0  # 朝向角,向东
-        elif random_r == 1 and random_b == 1:  # red随机,blue随机
-            red.ac_pos = np.append(np.random.uniform(-500, 500),
-                                   np.random.uniform(-250, 250))
-            red.ac_heading = np.random.uniform(0, 360)
-            blue.ac_pos = np.append(np.random.uniform(-500, 500),
-                                    np.random.uniform(-250, 250))
-            blue.ac_heading = np.random.uniform(0, 360)
         else:
-            raise Exception("random_r or random_b error")
+            raise Exception("random_r error")
+
+        if random_b == 1:
+            blue.ac_pos = np.append(np.random.uniform(-500, 500),
+                                    np.random.uniform(-250, 250))
+            blue.ac_heading = np.random.uniform(0, 360)
+        elif random_b == 0:
+            blue.ac_pos = np.array([-100.0, 0.0])  # 二维坐标
+            blue.ac_heading = 180  # 朝向角,向东
+        else:
+            raise Exception("random_b error")
     elif init_scen == 1:  # 进攻
-        if random_r == 0 and random_b == 1:  # red不随机,blue随机
-            red.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.append(np.random.uniform(-100, -500),
-                                    np.random.uniform(-50, 50))
-            blue.ac_heading = np.random.uniform(0, 30)
-        elif random_r == 1 and random_b == 0:  # red随机,blue不随机
-            red.ac_pos = np.append(np.random.uniform(-100, -500),
-                                   np.random.uniform(-50, 50))
-            red.ac_heading = np.random.uniform(0, 30)
-            blue.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            blue.ac_heading = 0  # 朝向角,向东
-        elif random_r == 1 and random_b == 1:  # red随机,blue随机
+        if random_r == 1:
             red.ac_pos = np.append(np.random.uniform(100, 500),
                                    np.random.uniform(-50, 50))
-            red.ac_heading = np.random.uniform(0, 30)
+            red.ac_heading = np.random.uniform(150, 210)
+        elif random_r == 0:
+            red.ac_pos = np.array([100.0, 0.0])  # 二维坐标
+            red.ac_heading = 0  # 朝向角,向东
+        else:
+            raise Exception("random_r error")
+
+        if random_b == 1:
             blue.ac_pos = np.append(np.random.uniform(-100, -500),
                                     np.random.uniform(-50, 50))
+            # 330~360 0~30
             blue.ac_heading = np.random.uniform(0, 30)
+        elif random_b == 0:
+            blue.ac_pos = np.array([-100.0, 0.0])  # 二维坐标
+            blue.ac_heading = 180  # 朝向角,向东
         else:
-            raise Exception("random_r or random_b error")
+            raise Exception("random_b error")
     elif init_scen == 2:  # 防守
-        if random_r == 0 and random_b == 1:  # red不随机,blue随机
-            red.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.append(np.random.uniform(100, 500),
-                                    np.random.uniform(-50, 50))
-            blue.ac_heading = np.random.uniform(0, 30)
-        elif random_r == 1 and random_b == 0:
+        if random_r == 1:
             red.ac_pos = np.append(np.random.uniform(100, 500),
                                    np.random.uniform(-50, 50))
+            # 330~360 0~30
             red.ac_heading = np.random.uniform(0, 30)
-            blue.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            blue.ac_heading = 0  # 朝向角,向东
-        elif random_r == 1 and random_b == 1:
-            red.ac_pos = np.append(np.random.uniform(-100, -500),
-                                   np.random.uniform(-50, 50))
-            red.ac_heading = np.random.uniform(0, 30)
-            blue.ac_pos = np.append(np.random.uniform(100, 500),
-                                    np.random.uniform(-50, 50))
-            blue.ac_heading = np.random.uniform(0, 30)
+        elif random_r == 0:
+            red.ac_pos = np.array([100.0, 0.0])  # 二维坐标
+            red.ac_heading = 180
         else:
-            raise Exception("random_r or random_b error")
-    elif init_scen == 3:  # 同向(面对面)
-        if random_r == 0 and random_b == 1:  # red不随机,blue随机
-            red.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.append(np.random.uniform(100, 500),
-                                    np.random.uniform(-50, 50))
-            blue.ac_heading = np.random.uniform(150, 180)
-        elif random_r == 1 and random_b == 0:
-            red.ac_pos = np.append(np.random.uniform(100, 500),
-                                   np.random.uniform(-50, 50))
-            red.ac_heading = np.random.uniform(150, 180)
-            blue.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            blue.ac_heading = 0  # 朝向角,向东
-        elif random_r == 1 and random_b == 1:
-            red.ac_pos = np.append(np.random.uniform(100, 500),
-                                   np.random.uniform(-50, 50))
-            red.ac_heading = np.random.uniform(150, 180)
+            raise Exception("random_r error")
+
+        if random_b == 1:
             blue.ac_pos = np.append(np.random.uniform(-100, -500),
                                     np.random.uniform(-50, 50))
-            blue.ac_heading = np.random.uniform(0, 30)
-        else:
-            raise Exception("random_r or random_b error")
-        pass
-    elif init_scen == 4:  # 中立(平行)
-        if random_r == 0 and random_b == 1:  # red不随机,blue随机
-            red.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.append(np.random.uniform(-50, 50),
-                                    np.random.uniform(-250, 250))
+            blue.ac_heading = np.random.uniform(150, 210)
+        elif random_b == 0:
+            blue.ac_pos = np.array([-100.0, 0.0])  # 二维坐标
             blue.ac_heading = 0
-        elif random_r == 1 and random_b == 0:
-            red.ac_pos = np.append(np.random.uniform(-50, 50),
-                                   np.random.uniform(-250, 250))
-            red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            blue.ac_heading = 0  # 朝向角,向东
-        elif random_r == 1 and random_b == 1:
-            red.ac_pos = np.append(np.random.uniform(-50, 50),
-                                   np.random.uniform(-250, 250))
-            red.ac_heading = 0  # 朝向角,向东
-            blue.ac_pos = np.array([0.0, 0.0])  # 二维坐标
-            blue.ac_heading = 0  # 朝向角,向东
         else:
-            raise Exception("random_r or random_b error")
+            raise Exception("random_b error")
+    elif init_scen == 3:  # 同向(面对面)
+        if random_r == 1:
+            red.ac_pos = np.append(np.random.uniform(100, 500),
+                                   np.random.uniform(-50, 50))
+            red.ac_heading = np.random.uniform(150, 210)
+        elif random_r == 0:
+            red.ac_pos = np.array([100.0, 0.0])  # 二维坐标
+            red.ac_heading = 180
+        else:
+            raise Exception("random_r error")
+
+        if random_b == 1:
+            blue.ac_pos = np.append(np.random.uniform(-100, -500),
+                                    np.random.uniform(-50, 50))
+            # 330~360, 0~30
+            blue.ac_heading = np.random.uniform(0, 30)
+        elif random_b == 0:
+            blue.ac_pos = np.array([-100.0, 0.0])  # 二维坐标
+            blue.ac_heading = 0
+        else:
+            raise Exception("random_b error")
+    elif init_scen == 4:  # 中立(平行)
+        if random_r == 1:
+            red.ac_pos = np.append(np.random.uniform(-50, 50),
+                                   np.random.uniform(-250, 250))
+            red.ac_heading = 0
+        elif random_r == 0:
+            red.ac_pos = np.array([0.0, 100.0])
+            red.ac_heading = 0
+        else:
+            raise Exception("random_r error")
+
+        if random_b == 1:
+            blue.ac_pos = np.append(np.random.uniform(-50, 50),
+                                   np.random.uniform(-250, 250))
+            blue.ac_heading = 0
+        elif random_b == 0:
+            blue.ac_pos = np.array([0.0, -100.0])
+            blue.ac_heading == 0
+        else:
+            raise Exception("random_b error")
     else:
         raise Exception("init_scen error")
     return red, blue
+
 
 # ===========================================
 #                state setting
